44079f8451170be3b21393db6e4dbd44c16beaaf
takuya <takuyamishima@gmail.com>
2018-08-03 10:43:05 +0000

    レビュー詳細表示まで実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def index
+    @review = Review.all
   end
   
   def new
+    @review = Review.new
   end
+
+  def create
+    Review.create(review_params)
+    redirect_to new_review_path
+  end
+  
+  def show
+  @review = Review.find(params[:id])
+  end
+  
+  private
+  def review_params
+    params.require(:review).permit(:title, :content)
+  end
+
 end

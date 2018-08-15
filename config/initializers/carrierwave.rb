f300c6a7708844cfe0b317bfe7b3d15d8ae36c28
degwinthegreat <the.colledge.dropout@gmail.com>
2018-08-15 14:41:45 +0900

    confirmでもキャッシュ化した画像が表示されるようにしました

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
+    
     config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['ACCESS_KEY_ID'],
         aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
         region: 'ap-northeast-1'#リージョンをUS以外にしたかたはそのリージョンに変更
     }
-
+    
+    config.cache_storage = :fog
     case Rails.env
     when 'development'
         config.fog_directory  = 'tamtim'

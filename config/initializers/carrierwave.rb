de157ea947ec9438d875d148fb792b15e99d665c
degwinthegreat <the.colledge.dropout@gmail.com>
2018-08-15 14:34:51 +0900

    amazon s3連携

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'#リージョンをUS以外にしたかたはそのリージョンに変更
+        provider: 'AWS',
+        aws_access_key_id: ENV['ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'#リージョンをUS以外にしたかたはそのリージョンに変更
     }
-  
-      case Rails.env
-      when 'development'
-          config.fog_directory  = 'tamtim'
-          config.asset_host = 'https://s3.amazonaws.com/tamtim'
-      when 'production'
-          config.fog_directory  = 'tamtim'
-          config.asset_host = 'https://s3.amazonaws.com/tamtim'
-      end
-  end
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'tamtim'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tamtim'
+    when 'production'
+        config.fog_directory  = 'tamtim'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tamtim'
+    end
+end

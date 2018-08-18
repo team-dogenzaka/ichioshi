f9d20915f58cc37de5e931d3ccb6f78b7fae3ed1
takuya <takuyamishima@gmail.com>
2018-08-18 05:51:58 +0000

    フォロー機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,4 +29,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit( :name, :icon )
   end
+  
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.followings
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,27 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :favorites, dependent: :destroy
   has_many :favorite_reviews, through: :favorites, source: :review
+  
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+  
   validates :name, presence: true, length: { maximum: 50 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
 
          def self.find_for_facebook_oauth(auth)
           user = User.where(uid: auth.uid, provider: auth.provider).first

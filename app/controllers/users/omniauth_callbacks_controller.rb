4af4f1c8d4309581e279eeff78c6463d354fac87
degwinthegreat <the.colledge.dropout@gmail.com>
2018-09-01 08:52:36 +0900

    ログイン後ユーザーページに遷移するよう変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController  
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      redirect_to  new_user_session_path, event: :authentication
       set_flash_message(:notice, :success, kind: "facebook") if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']

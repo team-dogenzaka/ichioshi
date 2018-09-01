d90186b0157a031f8b26d86550a2352c840885fd
degwinthegreat <the.colledge.dropout@gmail.com>
2018-09-01 18:10:03 +0900

    ユーザー編集を今のパスワードを入れなくてもできるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,18 @@ class User < ApplicationRecord
       end
     end
   end
+
+  # 今のパスワードを入力しなくてもユーザー情報を変更できるようにするメソッド
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+ 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+ 
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end

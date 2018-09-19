9646e4d55d36ee70da02e3c63433d06d7f0a60b7
takuya <takuyamishima@gmail.com>
2018-09-19 14:32:33 +0000

    TOPにフォローしているユーザーの新着レビューが表示される

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,10 @@ class HomesController < ApplicationController
   def index
     @users = User.all
     @review = Review.all
+    @user = current_user
+    if user_signed_in?
+    @feed_items = current_user.feed
+    else
+    end
   end
 end

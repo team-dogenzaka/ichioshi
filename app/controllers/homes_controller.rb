091ca415da6617adbb53031ddf1999f542533ce7
takuya <takuyamishima@gmail.com>
2018-10-06 03:16:49 +0000

    TOPのレビューを関数化

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
     @users = User.all
     @review = Review.all
     @user = current_user
+    @tag =  ActsAsTaggableOn::Tag.all
     if user_signed_in?
     @feed_items = current_user.feed
     else

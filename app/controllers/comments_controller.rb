bd2d32b87abf41546926232bf0b7be050cc81d3a
degwinthegreat <the.colledge.dropout@gmail.com>
2018-12-06 20:45:42 +0900

    ユーザ詳細が表示できるように

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  after_action :create_notifications, only: [:create]
 
   def create
     @review = Review.find(params[:review_id]) #①
@@ -9,7 +10,7 @@ class CommentsController < ApplicationController
       render :index #④
     end
   end
-  
+
   def destroy
     @review = Review.find(params[:review_id]) #①
     @comment = Comment.find(params[:id]) #⑤
@@ -19,8 +20,15 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:content, :review_id, :user_id)
+  def create_notifications_reviewer
+    return if @comment.user.id == current_user.id
+      Notification.create(user_id: @review.user.id,
+        notified_by_id: current_user.id,
+        review_id: @review.id,
+        notified_type: 'コメント')
     end
+  end
 
-end
+  def comment_params
+    params.require(:comment).permit(:content, :review_id, :user_id)
+  end

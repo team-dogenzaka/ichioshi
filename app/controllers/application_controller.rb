dc5a7a0ba968c1f2848107797ba76e21064aa9f7
degwinthegreat <the.colledge.dropout@gmail.com>
2018-12-22 21:12:17 +0900

    ユーザー画像の管理をシンプルに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:icon])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avator])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:coverimg])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:icon])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avator])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
     devise_parameter_sanitizer.permit(:account_update, keys: [:coverimg])
     devise_parameter_sanitizer.permit(:account_update, keys: [:tag])

b1a119d63caec0c87f18916a6e119ac3687fc63a
川井田新介 <macbook@kawaidashinsukenoMacBook.local>
2019-02-17 13:46:07 +0900

    パスワードの再設定ができるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_host
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
@@ -15,4 +16,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :profile, :icon,  :password, :coverimg, :tag) }
   end
 
+  def set_host
+    ActionMailer::Base.default_url_options = {:host => "https://" + request.host_with_port}
+  end
 end

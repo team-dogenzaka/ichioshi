391de06337ff42650fcbf7e418ee168b548448f7
degwinthegreat <the.colledge.dropout@gmail.com>
2020-11-17 00:48:16 +0900

    とりあえずローカルで画像添付してもエラーにならないようにする

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,14 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-  process resize_to_fit: [1900, 1080]
+  # include CarrierWave::MiniMagick
+  # process resize_to_fit: [1900, 1080]
   # Choose what kind of storage to use for this uploader:
-  storage :fog
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -13,7 +16,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:

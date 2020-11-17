53b13ddb825c451cb3356fac44ffd6d73b090d20
takuyamishima <takuyamishima@gmail.com>
2020-11-17 22:46:13 +0900

    Revert "ユーザー詳細ページにデフォルトの背景画像を設定"
    This reverts commit 43b0e193b3a052425681ec8579aa7b8f0fef0679.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 # Category.create(id: 9, category_name: "食・お酒")
 
 #ユーザー
- User.create(id: 999, email: 'hogehogehoge@gmail.com', password: 'hogehoge1234', uid: '2699356410203337', provider: 'facebook', name: 'Kouichi Tamate', icon: 'http://graph.facebook.com/v2.10/2699356410203337/picture?type=large', accepted: true)
+# User.create(id: 999, email: 'hogehogehoge@gmail.com', password: 'hogehoge1234', uid: '2699356410203337', provider: 'facebook', name: 'Kouichi Tamate', icon: 'http://graph.facebook.com/v2.10/2699356410203337/picture?type=large', accepted: true)
 
 #review
 # Review.create(title: '焚き火台', content: 'やはりキャンプには焚き火が欠かせない。が、意外に焚き木をするのは技術が必要。でもこのMONORALの焚き火台(ワイヤーフレーム)を使えば、簡単。しかも軽量で折畳み式なので、持ち運びも便利な優れもの', user_id: 999, image: "https://images-fe.ssl-images-amazon.com/images/I/5141-4iRK8L.jpg", category_name: 'スポーツ・レジャー')

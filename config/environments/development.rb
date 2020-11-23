65f0151d7b46480817114c33a4d250d3c56af943
degwinthegreat <the.colledge.dropout@gmail.com>
2020-11-23 12:24:48 +0900

    N+1に気づけるようにBulletを有効化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,13 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end

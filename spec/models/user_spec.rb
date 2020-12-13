23bfc6fc6664cea55cab4630c99642930f013309
degwinthegreat <the.colledge.dropout@gmail.com>
2020-12-13 14:38:11 +0900

    follow!とunfollow!のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,46 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '#follow!' do
+    let(:user) { create(:user) }
+    let(:followee) { create(:user) }
+
+    context 'フォローしていないユーザーだった場合' do
+      it '新しく追加されたレコードを返すこと' do
+        expect(user.follow!(followee))
+          .to have_attributes(follower_id: user.id, following_id: followee.id)
+      end
+    end
+
+    context 'フォローしているユーザーだった場合' do
+      before { create(:relationship, follower: user, following: followee) }
+
+      it 'フォローしているレコードを返すこと' do
+        expect(user.follow!(followee))
+          .to have_attributes(follower_id: user.id, following_id: followee.id)
+      end
+    end
+  end
+
+  describe '#unfollow!' do
+    let(:user) { create(:user) }
+    let(:unfollowee) { create(:user) }
+
+    context 'フォローしているユーザーだった場合' do
+      let!(:relationship) { create(:relationship, follower: user, following: unfollowee) }
+
+      it %w[削除したレコードを返すこと 対象のレコードが削除されていること].join(', ') do
+        expect(user.unfollow!(unfollowee))
+          .to have_attributes(follower_id: user.id, following_id: unfollowee.id)
+        expect { relationship.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'フォローしていないユーザーだった場合' do
+      it 'falseを返すこと' do
+        expect(user.unfollow!(unfollowee)).to be nil
+      end
+    end
+  end
 end
